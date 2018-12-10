f2520e7dc6d8668611e79204cd50743293eaf237
yyd <yyd102926@163.com>
2018-12-10 17:43:42 +0800

    自动化配置vuex文件

--- src/store/index.js
+++ src/store/index.js
@@ -3,17 +3,31 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex);
 
-import lefts from './modules/left'
+// import lefts from './modules/left'
+
+// const store = new Vuex.Store({
+//   state: {},
+//   mutations: {},
+//   actions: {},
+//   getters: {},
+//   modules: {
+//     lefts
+//   }
+// })
+
+// 自动化配置vuex文件
+var filsJs = require.context("./modules", true, /\.js$/);
+var modules = {};
+filsJs.keys().forEach((fileName) => {
+  modules[fileName.replace(/(\.\/|\.js)/g, '')] = filsJs(fileName).default
+});
 
 const store = new Vuex.Store({
   state: {},
   mutations: {},
   actions: {},
   getters: {},
-  modules: {
-    lefts
-  }
-
+  modules: modules
 })
 
 export default store
